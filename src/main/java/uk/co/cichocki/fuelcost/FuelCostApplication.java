4e20d36bb898b0e69bf2f778c999f5f399258208
Marcin Cichocki <marcin@cichocki.co.uk>
2019-04-26 19:12:06 +0100

    initial auto generated code

---
+++ src/main/java/uk/co/cichocki/fuelcost/FuelCostApplication.java
@@ -0,0 +1,13 @@
+package uk.co.cichocki.fuelcost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelCostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelCostApplication.class, args);
+	}
+
+}
