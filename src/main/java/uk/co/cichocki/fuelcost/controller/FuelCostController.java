078383dd5199dfed705200e82e6a8e6e6729c762
Marcin Cichocki <marcin@cichocki.co.uk>
2019-05-02 20:11:21 +0100

    error handling improved

--- src/main/java/uk/co/cichocki/fuelcost/controller/FuelCostController.java
+++ src/main/java/uk/co/cichocki/fuelcost/controller/FuelCostController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,8 @@ import uk.co.cichocki.fuelcost.model.Journey;
 import uk.co.cichocki.fuelcost.service.FuelCostService;
 
 import javax.validation.Valid;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
@@ -37,8 +40,20 @@ public class FuelCostController {
     @SuppressWarnings("unused") // intellij annoyance
     @ExceptionHandler(HttpMessageNotReadableException.class)
     protected ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
-        log.error("Handling HttpMessageNotReadableException", ex);
-        ErrorResponse errorResponse = new ErrorResponse("Problem occurred while processing message", ex.getMessage());
+        log.error("Handling HttpMessageNotReadableException");
+        String message = Optional.ofNullable(ex.getCause()).map(t -> t.getClass().getSimpleName()).orElse("Invalid json");
+
+        ErrorResponse errorResponse = new ErrorResponse("Problem occurred while processing message", message);
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @SuppressWarnings("unused")
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.error("handling MethodArgumentNotValidException");
+        var message = ex.getBindingResult().getAllErrors().stream().map(
+                err -> err.getObjectName() + ": " + err.getDefaultMessage()
+        ).collect(Collectors.joining(System.lineSeparator()));
+        return new ResponseEntity<>(new ErrorResponse("Validation failed", message), HttpStatus.BAD_REQUEST);
     }
 }
